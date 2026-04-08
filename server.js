911b19fac0aeff1199ea606ac458c9a4ae85702e
MR_BESSEM <baatour.soft@gmail.com>
2026-04-08 22:24:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -687,7 +687,7 @@ function signAliExpressRestRequest(apiPath, params, secret) {
     .sort()
     .map((key) => `${key}${params[key]}`)
     .join("");
-  return crypto.createHash("sha256").update(`${normalizedPath}${sorted}${secret}`, "utf8").digest("hex").toUpperCase();
+  return crypto.createHash("sha256").update(`${normalizedPath}${sorted}`, "utf8").digest("hex").toUpperCase();
 }
 
 async function createAliExpressAccessToken(code) {
