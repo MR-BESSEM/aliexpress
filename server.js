2ac6f142470803fb8ee7142f084300e10e30a263
MR_BESSEM <baatour.soft@gmail.com>
2026-04-11 23:33:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3282,7 +3282,7 @@ await page.waitForTimeout(3000).catch(() => {});
     await context.close().catch(() => {});
     await browser.close().catch(() => {});
   }
-
+} // ✅ THIS LINE FIXES EVERYTHING
 
 async function scrapeWithPlaywright(url) {
   return scrapeAliExpressWithScrapingDog(url, "scrapingdog");
